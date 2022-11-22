da268f826ee8192121fcc2f222371b7f2e31ec9a
Hunter Wood <hunter674530@gmail.com>
2022-11-22 11:07:35 -0600

    commit tree 721fd1c2039746d484f14b6098f0835493f9efbf

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.body.removeChild(main);
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Joe is the champion";
